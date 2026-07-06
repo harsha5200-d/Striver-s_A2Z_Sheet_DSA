791fbef6b7405364d3b657708af5ba0c22702645
harsha <hreshavena@gmail.com>
2026-07-06 12:46:03 +0530

    pattern20

---
+++ patterns/patterns20.py
@@ -0,0 +1,32 @@
+def patterns19(n):
+
+    for i in range(n):
+
+        for j in range(i+1):
+            print("*",end="")
+        
+        for j in range((n-(i+1))*2):
+            print(" ",end="")
+
+        for j in range(i+1):
+            print("*",end="")
+        
+        print()
+
+    
+    for i in range(n-2,-1,-1):
+
+        for j in range(i+1):
+            print("*",end="")
+        
+        for j in range((n-(i+1))*2):
+            print(" ",end="")
+
+        for j in range(i+1):
+            print("*",end="")
+        
+        print()
+    
+
+
+patterns19(5)
