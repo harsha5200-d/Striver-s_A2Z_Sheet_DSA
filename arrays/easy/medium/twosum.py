3f025eff24be87d86fa7a945307e3f8624fbbe8f
harsha <hreshavena@gmail.com>
2026-07-10 17:18:08 +0530

    twosum by brute force

--- arrays/easy/medium/twosum.py
+++ arrays/easy/medium/twosum.py
@@ -1,3 +1,14 @@
-def twosum(arr):
+def twosumByBruteForce(arr,target):
 
-    
+    for i in range(len(arr)):
+
+        for j in range(i+1,len(arr)):
+
+            if(arr[i]+arr[j]==target):
+                return True
+    return False
+
+
+arr = list(map(int,input().split()))
+res = twosumByBruteForce(arr,70)
+print(res)
