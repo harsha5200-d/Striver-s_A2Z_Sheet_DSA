8fc94c27824ae1451951d08fac4e2ad5a584edfd
harsha <hreshavena@gmail.com>
2026-07-10 17:49:56 +0530

    twosum by better approach

--- arrays/easy/medium/twosum.py
+++ arrays/easy/medium/twosum.py
@@ -8,7 +8,20 @@ def twosumByBruteForce(arr,target):
                 return True
     return False
 
+def twosumbyMap(arr,target):
+
+    map = {}
+    for i in range(len(arr)):
+
+        if target-arr[i] in map:
+            return [i,map[target-arr[i]]]
+        
+        map[arr[i]] = i
+        
+
+    return [-1,-1]
 
 arr = list(map(int,input().split()))
-res = twosumByBruteForce(arr,70)
+res = twosumbyMap(arr,8)
 print(res)
+
