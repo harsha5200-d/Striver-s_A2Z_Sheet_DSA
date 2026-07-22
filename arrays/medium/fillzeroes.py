c4ff51392b8871e6dccce1bb6a5432862457ed0a
harsha <hreshavena@gmail.com>
2026-07-22 09:15:04 +0530

    fillzeroes

--- arrays/medium/fillzeroes.py
+++ arrays/medium/fillzeroes.py
@@ -8,6 +8,31 @@ def fillzeroes(matrix):
         for j in range(m):
 
             if(matrix[i][j]==0):
+
+                for row in range(m):
+
+                    if(matrix[i][row]!=0):
+                        matrix[i][row]=-1
+
+                for col in range(n):
+
+                    if(matrix[col][i] != 0):
+                        matrix[col][i] = -1
+
+
+    for i in range(n):
+        for j in range(m):
+            if(matrix[i][j]==-1):
+                matrix[i][j]=0
+
+
+    return matrix
+
+matrix=[[1,1,1],[1,0,1],[1,1,1]]
+
+res = fillzeroes(matrix)
+
+print(res)
                 
                 
 
