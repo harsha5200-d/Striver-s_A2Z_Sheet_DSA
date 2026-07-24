479f73720680880a9e737f8e503cb2e3e428f3a1
harsha <hreshavena@gmail.com>
2026-07-24 08:05:08 +0530

    trees traversel

---
+++ BinaryTree/Traversels/postorder.py
@@ -0,0 +1,42 @@
+class Tree:
+
+    def __init__(self,data):
+
+        self.left = None
+        self.data = data
+        self.right = None
+
+
+def buildTree(arr,i):
+
+    if(len(arr)<=i):
+        return 
+    
+
+    root = Tree(arr[i])
+    root.left = buildTree(arr,i*2+1)
+    root.right = buildTree(arr,i*2+2)
+
+
+    return root
+
+def postorder(root):
+
+    if not root:
+        return None
+
+    postorder(root.left)
+    postorder(root.right)
+    print(root.data,end=" ")
+
+
+
+arr = [1, 2, 3, 4, 5, 6, 7]
+root = buildTree(arr,0)
+postorder(root)
+
+
+
+
+
+    
