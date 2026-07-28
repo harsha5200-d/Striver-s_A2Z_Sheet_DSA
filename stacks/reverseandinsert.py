c88cb7c24c975850437636f78a21077bfc3f3c60
harsha <hreshavena@gmail.com>
2026-07-28 10:08:51 +0530

    stacksstarted

---
+++ stacks/reverseandinsert.py
@@ -0,0 +1,25 @@
+def insert_bottom(stack, x):
+    
+    if len(stack) == 0:
+        stack.append(x)
+        return
+
+    temp = stack.pop()
+    insert_bottom(stack, x)
+    stack.append(temp)
+
+
+def reverse_stack(stack):
+    if len(stack) == 0:
+        return
+
+    temp = stack.pop()
+    reverse_stack(stack)
+    insert_bottom(stack, temp)
+
+
+stack = [1, 2, 3, 4]
+
+reverse_stack(stack)
+
+print(stack)
