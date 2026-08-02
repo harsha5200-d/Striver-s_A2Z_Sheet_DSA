bcb2e2c90c2654c4dd6db1cfe8b0c2fc79cf207b
harsha <hreshavena@gmail.com>
2026-08-02 19:51:04 +0530

    Add logic comments to ArmStrong.py

--- Basic_Math/ArmStrong.py
+++ Basic_Math/ArmStrong.py
@@ -1,15 +1,19 @@
 def ArmStrong(num):
 
+    # Initialize sum to store the sum of powers of digits
     sum = 0
     temp = num
+    # Find the number of digits
     k = len(str(num))
 
+    # Extract digits and add their k-th power to sum
     while temp > 0:
 
         digit = temp%10
         sum += digit**k
         temp //= 10 
     
+    # Check if sum is equal to the original number
     if sum==num:
         return True
     
