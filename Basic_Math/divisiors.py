7be8c11779d3c180947c912aabffaf3d936a30bd
harsha <hreshavena@gmail.com>
2026-08-02 19:51:45 +0530

    Add logic comments to divisiors.py

--- Basic_Math/divisiors.py
+++ Basic_Math/divisiors.py
@@ -3,14 +3,18 @@ import math
 
 def divisiors(num):
     res = []
+    # Loop from 1 to the square root of the number
     for i in range(1,int(math.sqrt(num))+1):
 
+        # If i is a divisor
         if(num%i==0):
             res.append(i)
 
+            # Check for the paired divisor
             if (i != num//i):
                 res.append(num//i)
         
+    # Return the divisors in sorted order
     return sorted(res)
 
 resu = divisiors(36)
