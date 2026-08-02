74703135f359d5745034a6ce04902630805d01ae
harsha <hreshavena@gmail.com>
2026-08-02 19:51:20 +0530

    Add logic comments to GCD.py

--- Basic_Math/GCD.py
+++ Basic_Math/GCD.py
@@ -1,7 +1,9 @@
 def gcd(n1,n2):
 
+    # Iterate from the minimum of the two numbers down to 1
     for i in range(min(n1,n2),0,-1):
 
+        # Check if i divides both numbers without a remainder
         if(n1%i==0 and n2%i==0):
             return i
         
